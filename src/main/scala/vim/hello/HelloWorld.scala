693f6f2be5641962f4eff9b0665594345f5c6fe3
Vitthal Mirji <vitthalmirji@gmail.com>
2025-09-12 17:04:42 +0530

    init

---
+++ src/main/scala/vim/hello/HelloWorld.scala
@@ -0,0 +1,21 @@
+package vim.hello
+
+import org.apache.spark.sql.SparkSession
+
+/**
+ * Hello world: Quick apache spark setup using scala 3
+ */
+
+object HelloWorld:
+  @main def run(): Unit =
+    val spark = SparkSession.builder
+      .appName("HelloWorld")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    import spark.implicits._
+    val df = List("hello", "world").toDF
+    df.show()
+
+    spark.stop
